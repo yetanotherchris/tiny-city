bcea6db3e8d72f83c3b280b220566d108718b20f
Chris <frown-wizard-hug@duck.com>
2025-04-20 00:28:07 +0100

    Ignore duplicate bookmarks (name & url are identical)

--- Model/BookmarkNode.cs
+++ Model/BookmarkNode.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace TinyCity.Model
 {
-    public class BookmarkNode
+    public class BookmarkNode : IEquatable<BookmarkNode>
     {
         [JsonPropertyName("date_added")]
         public string? DateAdded { get; set; }
@@ -24,5 +24,21 @@ namespace TinyCity.Model
 
         [JsonPropertyName("children")]
         public List<BookmarkNode>? Children { get; set; }
+
+        public bool Equals(BookmarkNode? other)
+        {
+            if (other is null) return false;
+            return Url == other.Url && Name == other.Name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as BookmarkNode);
+        }
+
+        public override int GetHashCode()
+        {
+            return Url?.GetHashCode() ?? 0;
+        }
     }
 }
