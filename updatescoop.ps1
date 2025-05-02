9f675d7a58b66187c827d54c19dab04cf0b6623e
Chris <frown-wizard-hug@duck.com>
2025-05-02 22:19:40 +0100

    May as well add scoop.sh support

---
+++ updatescoop.ps1
@@ -0,0 +1,30 @@
+param(
+	[Parameter(Mandatory=$true)]
+	[String]
+	$version
+)
+
+$search = "*publish\tinycity.exe"
+$tinyCityFile = Get-ChildItem -Path "./bin/Release/" -Force -Recurse -File | Where-Object { $_.FullName -like $search }
+$filePath = $tinyCityFile[0].FullName
+
+Write-Output "File found: $filePath, getting hash..."
+$hash = (Get-FileHash -Path $filePath -Algorithm SHA256).Hash
+Write-Output "Hash: $hash"
+
+Write-Output "Creating tinycity.json for version $version..."
+Write-Output "{
+  `"version`": `"$version`",
+  `"architecture`": {
+      `"64bit`": {
+          `"url`": `"https://github.com/yetanotherchris/tiny-city/releases/download/v$version/tinycity.exe`",
+          `"bin`": [
+              `"tinycity/tinycity.exe`"
+          ],
+          `"hash`": `"$hash`"
+      }
+  },
+  `"homepage`": `"https://github.com/yetanotherchris/tiny-city`",
+  `"license`": `"MIT License`",
+  `"description`": `"A console app written that lists and searches your bookmarks files`"
+}" | Out-File -FilePath "tinycity.json" -Encoding utf8
